1d9bcd63604b23856c6c79a880428be19fa083da
dorsi-alt <orsidaniel03@gmail.com>
2021-07-01 01:34:27 -0400

    updates to each page

--- src2/app.js
+++ src2/app.js
@@ -1,7 +1,11 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const map= document.querySelector('#map')
+
 
 menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+
